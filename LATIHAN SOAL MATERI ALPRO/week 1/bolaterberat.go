59c6917bd45614bc4a567066974267c88ebe475b
Estetika Ananda Poetri Hariyanto <estetika0306@gmail.com>
2024-10-28 14:35:08 +0700

    Delete LATIHAN SOAL MATERI ALPRO directory

--- LATIHAN SOAL MATERI ALPRO/week 1/bolaterberat.go
+++
@@ -1,9 +0,0 @@
-Berikut langkah singkat untuk menemukan bola terberat:
-
-1. Timbang Bola A vs Bola B:
-- Jika seimbang, lanjut ke Bola C vs Bola D.
-- Jika tidak seimbang, timbang Bola A vs Bola C.
-
-2. Hasil:
-- Jika Bola C seimbang dengan Bola A, maka Bola B yang terberat.
-- Jika tidak seimbang, bola yang lebih berat di timbangan terakhir adalah yang terberat.
